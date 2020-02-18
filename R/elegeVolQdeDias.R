34efc8429dc261686f5068a23988ca4499bb1c4f
kleberformiga <mirandakf@ufersa.edu.br>
2020-02-18 11:46:13 -0300

    Finalização do banco de papeis e ano classificados para portfolio com base em volume e quantidade de dias de negociação em bolsa

--- R/elegeVolQdeDias.R
+++ R/elegeVolQdeDias.R
@@ -1,52 +1,93 @@
-# Geracao da base de dados e filtro de volume e
-# quantidade de dias de negociação
+#########################################################################
+# GERAÇÃO DA BASE DE DADOS                                              #
+# Filtro por volume e liquidez (dias negociados)                        #
+# Inputs:                                                               #
+#        1) Planilha com quantitativo de dias negociados na B3          #
+#        2) Planilha com preço de fechamento e volume diário            #
+# Outputs:                                                              #
+#        1) Banco de dados com código e ano dos papeis com maior volume #
+#           por empresa que poderão compor o portfolio, baseado na      #
+#           quantidade de dias negociados e volume médio anual          #
+#########################################################################
 
 # Instalando as funcoes do contabiliDados
 source("https://raw.githubusercontent.com/kleberformiga/contabilidados/master/contabilidados.R")
 cntdd.carregaPacotes("tidyverse")
 
-# Montagem das bases de dados
+# Montagem das bases de dados ####
 
-# Ver dias de negociacao por periodo
+  # Quantidade de pregões na B3 por ano
 
-ddNeg <- readRDS("G:/Meu Drive/criaCarteiras/R/dados/diasNeg.rds") %>%
-  mutate(lagQdeDiasTot = shift(qdeDiasTot)) %>% na.omit
+  ddNeg <- readRDS("G:/Meu Drive/criaCarteiras/R/dados/diasNeg.rds") %>% 
+  mutate(ano = as.integer(ano))
 
-cntdd.uneMatrix(Arquivo = "G:/Meu Drive/criaCarteiras/R/dados/fechDiarioport.xlsx",
-                SeqVarPlan = c("preco", "volume"),
-                index = c("cod", "data"),
-                clsPer = "date", clsVlr = "numeric")
-
-# Parâmetros para filtro
-percdias <- 80
-parVolume <- 500000
-
-
+  # Baixa dados de preço (fechamento) e volume com função apropriada
+  # para matrix do Economatica
   
-  # Baixando a quantidade de dias de negociacao
+  cntdd.uneMatrix(
+    Arquivo = "G:/Meu Drive/criaCarteiras/R/dados/fechDiarioport.xlsx",
+    SeqVarPlan = c("preco", "volume"),
+    index = c("cod", "data"),
+    clsPer = "date", clsVlr = "numeric"
+  )
 
+  gc()
+  
+  
+# Preparando o filtro com percentual de dias negociados no ano anterior
+# e volume diário do ano anterior
+
+  # Parâmetros
+    param.volume <- 3100
+    param.percdias <- 0.01
   
   # Elegibilidade por:
-  #     1) acao mais negociada (quando tiver duas)
-  #     2) acao com mais de 80% de dias negociados no ano anterior
-  #     3) acao com volume maior que 500.000, por dia, no ano anterior
+  #  1) acao mais negociada (quando tiver duas por empresa)
+  #  2) acao com mais de x% (percdias) de dias negociados no ano anterior
+  #  3) acao com volume diário médio maior que 500.000 no ano anterior (parVolume)
   
-  papeis <-
-    bdPainel %>%  
-    separate(data, c("ano", "mes", "dia")) %>%
+    # Indica o banco de dados
+    bdPainel %>%
+    
+    # Divide a data em três colunas: ano, mês e dia. Depois elimina a coluna dia
+    separate(data, c("ano", "mes", "dia"), convert = T) %>% select(-dia) %>% 
+    
+    # Elimina os valores nulos
     na.omit %>%
-    group_by(cod, ano) %>% 
-    summarise(qdeDias = n(), volumeMedio = mean(volume, na.rm = T)) %>%
-    group_by(cod) %>% 
-    mutate(lagQdeDias = shift(qdeDias), lagVolume = shift(volumeMedio)) %>% 
-    # criar condicao para escolher, dentre dois ou mais papeis, o com maior liquidez
-    mutate(cod2 = substr(cod, 1, 4)) %>% na.omit %>% 
-    group_by(cod2, ano) %>% 
-    mutate(elege = lagVolume == max(lagVolume, na.rm = T)) %>%
-    ungroup %>% 
-    left_join(select(diasNeg, ano, lagQdeDiasTot), by = "ano") %>%
-    mutate(percDiasNeg = lagQdeDias / lagQdeDiasTot * 100) %>%
-    filter(elege == T) %>% 
-    filter(percDiasNeg >= percdias & volumeMedio >= parVolume) %>% 
-    na.omit %>% select(cod, ano, elege, percDiasNeg, volumeMedio)
-  
+    
+    # Agrupa os dados por codigo e por ano
+    group_by(cod, ano) %>%
+      
+    # Calcula a quantidade de dias negociados e o volume diário médio
+    # por código e por ano (grupo)
+    summarise(qdeDias = n(), volumeMedio = mean(volume, na.rm = T)) %>% 
+      
+    # Une a base de dados com a base de dias de negociação da B3
+    left_join(ddNeg, by = "ano") %>% 
+      
+    # Agrupa por empresa e por ano
+    ungroup %>% group_by(empresa = substr(cod, 1, 4), ano) %>% 
+    
+    # Verifica qual dos papeis possui maior volume no ano anterior
+    mutate(v.maxVol = volumeMedio == max(volumeMedio, na.rm = T)) %>%
+    
+    # Agrupa por papel
+    ungroup %>% group_by(cod) %>% 
+    
+    # Verifica se o volume do ano anterior está dentro do parâmetro
+    mutate(v.qdeVol = lag(volumeMedio) > param.volume) %>%
+    
+    # Verifica se o percentual de dias negociados no ano anterior
+    # está dentro do parâmetro
+    mutate(v.qdeDD = lag(qdeDias)/lag(qdeDiasTot) > param.percdias) %>% ungroup %>%
+    
+    # Idenfifica qual papel atende todos os parâmetros com o respectivo período
+    mutate(atende = rowSums(select(., starts_with("v.")))) %>%
+      
+    # Filtra os papeis que atendem aos três parâmetros propostos
+    filter(atende == 3) %>%
+      
+    # Seleciona código do papel e o ano
+    select(cod, empresa, ano) -> elege.qdeVol
+    
+    
